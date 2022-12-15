e99b3b88ea221e8b2c470edc9e901a853a14717f
shubhamj-lanatus <shubhamj@lanatussystems.com>
2022-12-15 13:01:19 +0530

    change after install script for appspec.yml

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,4 +7,8 @@ cd /home/ubuntu/server
 # npm install pm2 -g
 # npm install serve -g
 
-sudo cp /home/ubuntu/server -r /root/BigBeyond/big-beyond-admin-v1
+sudo cp /root/BigBeyond/big-beyond-admin-v1 -r /root/BigBeyond/big-beyond-admin-archive
+
+sudo rm -r /root/BigBeyond/big-beyond-admin-v1
+
+sudo cp /home/ubuntu/server/ -r /root/BigBeyond/big-beyond-admin-v1/
